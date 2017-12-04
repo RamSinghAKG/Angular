378c8ba9b22362111e4a46491ab2c788f7139648
Ram Singh <ram.singh@zivame.com>
2017-12-04 21:17:04 +0530

    WIP routing

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
