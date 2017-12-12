f14a3f2f5f502020747ad942fd68f83bfb521414
Ram Singh <ram.singh@zivame.com>
2017-12-12 21:28:23 +0530

    added add item event in product component

--- app1/src/app/product/product.component.ts
+++ app1/src/app/product/product.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+    this.items = ['Angular 4', 'React JS', 'Underscore'];
+    this.newItem = "";
+   }
+   pushItem = function(){
+     if(this.newItem != ""){
+       this.items.push(this.newItem);
+       this.newItem = "";
+     }
+   }
   ngOnInit() {
   }
 
